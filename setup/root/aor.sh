16ec3dc33dc53c9a07b4a3ceb8c162a0c27a57ef
Jay Shridharani <jshridha@gmail.com>
2017-05-29 10:32:32 -0400

    Temp fix for nzbget requiring openssl 1.1

--- setup/root/aor.sh
+++ setup/root/aor.sh
@@ -7,5 +7,6 @@ set -e
 aor_packages="nzbget"
 
 # download and install package
-curl -L -o "/tmp/$aor_packages.tar.xz" "https://www.archlinux.org/packages/community/x86_64/$aor_packages/download/"
+#curl -L -o "/tmp/$aor_packages.tar.xz" "https://www.archlinux.org/packages/community/x86_64/$aor_packages/download/"
+curl -L -o "/tmp/$aor_packages.tar.xz" "https://archive.archlinux.org/repos/2017/04/13/community/os/x86_64/nzbget-18.0-1-x86_64.pkg.tar.xz"
 pacman -U "/tmp/$aor_packages.tar.xz" --noconfirm
